2584ca0ba64c6b829197b120112a44be8e3d4c5a
Romina Johari <166646614+rominajhr@users.noreply.github.com>
2024-11-01 23:53:21 +0330

    Create BigNumber.h

---
+++ BigNumber/BigNumber.h
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+#ifndef BIGNUMBER_H
+#define BIGNUMBER_H
+
+class BigNumber{
+    int size;
+    int digits[500];
+
+    public:
+        BigNumber();
+        BigNumber(string);
+        BigNumber(int);
+
+        void shiftR();
+        void shiftR(int);
+        void shiftL();
+        void shiftL(int);
+
+        BigNumber sum(BigNumber);
+        BigNumber minus(BigNumber);
+};
+
+#endif
